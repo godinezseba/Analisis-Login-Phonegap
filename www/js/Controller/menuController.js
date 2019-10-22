aa78b958039d8be2e3fea777a566f1293addb0ba
godinezseba <sebastian.godinez@sansano.usm.cl>
2019-10-22 03:32:18 -0300

    funciona el menu :D

--- www/js/Controller/menuController.js
+++ www/js/Controller/menuController.js
@@ -21,17 +21,17 @@ var menu = {
     datos: function () {
         $.ajax({
             type: "GET",
-            url: "http://localhost:8000/v1/torneo/getAll",
-            // dataType: "json",
+            url: "https://chile-2.herokuapp.com/v1/torneo/getAll",
+            dataType: "json",
+            xhrFields: {
+                withCredentials: false
+            },
             success: function(data) {
-                window.console.log("hola");
-                window.console.log(data);
                 data.forEach(element => {
                     $("#torneos").append('<div class="card border-primary mb-3" style="max-width: 18rem;">\
-                                            <div class="card-header">Header</div>\
+                                            <div class="card-header">'+element.nombre+'</div>\
                                             <div class="card-body text-primary">\
-                                                <h5 class="card-title">Primary card title</h5>\
-                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>\
+                                                <h5 class="card-title">Pais: '+element.pais+'</h5>\
                                             </div>\
                                         </div>');
                 });
